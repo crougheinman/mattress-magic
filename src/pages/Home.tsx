1c262088cc35ab7ac63642d6e2b31e113ff21e5f
Greg Peters <gregorypeteryanoc.gpy@gmail.com>
2026-06-04 04:44:47 +0800

    Implement Reveal component for animated scroll effects and integrate into Home and Financing pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,15 @@ import Features from '../components/Features';
 import Brands from '../components/Brands';
 import Layout from '../Layout';
 import About from '../components/About';
+import Reveal from '../components/Reveal';
 
 const Home = () => {
     return (
         <Layout title="Home">
-            <Hero />
-            <Features />
-            <About />
-            <Brands />
+            <Reveal><Hero /></Reveal>
+            <Reveal><Features /></Reveal>
+            <Reveal><About /></Reveal>
+            <Reveal><Brands /></Reveal>
         </Layout>
     );
 };
